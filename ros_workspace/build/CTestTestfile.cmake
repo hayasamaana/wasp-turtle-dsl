235d1cf10b2cbf0d8a94c9c6e4f9f8dc13f1ac9d
Christian Nelson <christian.nelson@eit.lth.se>
2017-09-05 22:38:42 +0200

    Initiated ROS workspace.

---
+++ ros_workspace/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/wasp-turtle-dsl/ros_workspace/src
+# Build directory: /home/ubuntu/wasp-turtle-dsl/ros_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ ros_workspace/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/wasp-turtle-dsl/ros_workspace/devel:/opt/ros/kinetic"
+export LD_LIBRARY_PATH="/opt/ros/kinetic/lib"
+export PWD="/home/ubuntu/wasp-turtle-dsl/ros_workspace/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/wasp-turtle-dsl/ros_workspace/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ubuntu/wasp-turtle-dsl/ros_workspace/src:/opt/ros/kinetic/share"
