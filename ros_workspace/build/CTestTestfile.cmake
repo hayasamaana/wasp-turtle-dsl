ffe195c981e3c50c3ee9a09297bad9ef1f95c14f
Christian Nelson <christian.nelson@eit.lth.se>
2017-09-05 22:54:22 +0200

    Stopped tracking build files.

--- ros_workspace/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/wasp-turtle-dsl/ros_workspace/src
-# Build directory: /home/ubuntu/wasp-turtle-dsl/ros_workspace/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
