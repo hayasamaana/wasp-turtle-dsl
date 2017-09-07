ee404a617bcc580532ad39d6259fa0cf1ed58e12
Christian Nelson <christian.nelson@eit.lth.se>
2017-09-07 22:21:06 +0200

    Now all parts are working.

---
+++ run_simulator.zsh
@@ -0,0 +1,14 @@
+#! /usr/bin/zsh
+cp LTH1_Turtle/generated_mission.py ros_workspace/src/lth1_turtle_dsl/src/generated_mission.py
+
+chmod +x ros_workspace/src/lth1_turtle_dsl/src/generated_mission.py
+
+cd ros_workspace
+
+catkin_make
+
+cd ..
+
+source ./ros_workspace/devel/setup.zsh
+
+roslaunch lth1_turtle_dsl run_generated.launch
